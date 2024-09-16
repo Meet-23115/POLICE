251fb5eb72f70f3ec06308233d0d4e6388fc9f0d
Meet Gelothra <imailmeet4000@gmail.com>
2024-09-17 00:27:11 +0530

    raand

---
+++ police/components/Nav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+    const Nav=()=>{
+return(
+    <>
+ajay ki shalu randi hai    
+    
+    
+    
+    
+    </>
+)
+ }
+    export default Nav;
---
+++ police/pages/signup/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+      ajay ki shaalu raand hai
+    signup 
+    <div className=' h-44 flex items-center justify-center border border-black left-3' >
+        sign up
+    </div>
+    </div>
+  )
+}
+
+export default index;
