7e7475bc1ce3483de9d91449ed9d48762f9d5668
ajaybagru <akv10192@gmail.com>
2024-09-17 00:30:53 +0530

    chutiya

---
+++ police/components/Nav.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+const Nav=()=>{
+    return(
+      <>
+      
+      <ul className=' flex justify-between bg '>
+        <li>home</li>
+        <li>about us </li>
+        <li>dashboard</li>
+        <li>contact us</li>
+      </ul>
+      </>
+
+    )
+  }
+  export default Nav;
--- police/pages/_document.tsx
+++ police/pages/_document.tsx
@@ -1,3 +1,4 @@
+import Nav from "@/components/Nav";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -5,7 +6,10 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body className="antialiased">
+        
+       
         <Main />
+        <Nav/>
         <NextScript />
       </body>
     </Html>
